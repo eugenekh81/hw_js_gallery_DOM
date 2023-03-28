4783d1edd07830c4fa40d7342f041dafc4c5d5ec
Alex <kartanovaleks@gmail.com>
2023-03-28 12:11:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const largeImg = document.querySelector("#largeImg");
+
+const smallImges = document.querySelectorAll(".gallery__img gallery__thumb");
+
+const list = document.querySelector("ul");
+
+list.addEventListener("click", (event) => {
+  event.preventDefault();
+  const largeImgSrc = event.target.closest(".list-item__link").href;
+  largeImg.src = largeImgSrc;
+});
